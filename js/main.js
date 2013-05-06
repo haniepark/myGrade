a2cf3ba7f1acd0b1439b0da6c013d6699e487c20
Patrick Kang <jykang1@gmail.com>
2013-05-06 00:52:23 -0400

    add plus minus to control number of entries

--- js/main.js
+++ js/main.js
@@ -25,4 +25,25 @@ $(function(){ // In jQuery 1.6+ this is the same as $(document).ready(function()
 		}
 	});
 
+
+
+	// adding/subtracting mark entries.
+
+	// initializing all numbEntries to 1.
+	$('.entries').each(function(){
+		$(this).data("numEntries", 1);
+	});
+
+
+	// if button is clicked, change numEntries.
+	$('.btn').on('click', function(){
+		var $sec = $(this).closest('.entries');
+		var $numEntries = $sec.data("numEntries") + 1;
+		var $entry = $('<label class="entry">Midterm '+$numEntries+' : <input class="midterms"/></label>');
+		$(this).closest('.entry').append($entry);
+		$sec.data("numEntries", $numEntries);
+	});
+
+
+
 });
